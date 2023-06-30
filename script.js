050eb6d8ce28b7742c56d91ce7d67038b4af77b3
Jamyle Layla S. Teles <104221009+JamyleTeles@users.noreply.github.com>
2023-06-30 20:33:19 -0300

    Resposta_Código

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ function primo(){
     var numero = parseInt(document.getElementById('numero').value);
     var resposta = document.getElementById('resposta');
     var divisores=0;
-  
-    for(var count=1 ; count<=numero ; count++)
-     if(numero % count == 0)
-         divisores++;
     
-    if(divisores==2)
-        resposta.innerHTML='É primo';
-    else
-        resposta.innerHTML='Não é primo';
-  }
+if (numero <= 0){
+    resposta.innerHTML = 'Não é possível verificar se este número é primo.';
+} else {
+        for(var count=1 ; count<=numero ; count++){
+            if(numero % count == 0){
+                divisores++;
+            
+                if(divisores==2){
+                    resposta.innerHTML='O número é primo';
+                } else {
+                    resposta.innerHTML='O número não é primo';
+                }
+            }
+        }
+    }
+}
